abf635b44e9fc914d858485c59aa2e8a5458d35a
NOED <38118524+N-O-E-D@users.noreply.github.com>
2020-02-29 22:12:58 +0100

    Update download.js
    Bug fixed

--- download.js
+++ download.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TheIshter-script
+// @name         Theishter-script
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  download PDFs from http://www.theishter.com/sheet-music.html with a button
@@ -18,7 +18,7 @@ window.onload=function() {
     var string;
     var links=document.getElementsByTagName('a');
     for(let item of links) {
-        if(item.href.toString().includes("drive.google.com")){
+        if(item.href.toString().includes("drive.google.com") && !item.href.toString().includes("&export=download")){
 
             if(item.href.toString().includes("open")){
                 id=(item.href.toString().split("/")[3]).replace("open?id=","");
@@ -28,14 +28,17 @@ window.onload=function() {
              }
             count++;
             dwnld=downloadprefix+id+"&export=download";
-            string="<a href='"+dwnld+"'>   <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQe0yTrscxvCtQSSYecldTwJhb446m5cG2BGRy4gsa2AEKBJWQR' width='30'></img></a>";
-            console.log(string);
+            //console.log(dwnld);
+            //console.log(dwnld);
+            //string="<a href="+dwnld+">   <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQe0yTrscxvCtQSSYecldTwJhb446m5cG2BGRy4gsa2AEKBJWQR' width='30'></img></a>";
+            //dwnld="https://www.google.it";
+            //dwnld="https://www.google.it";
+            string="<a class='_randomClass_name' href='"+dwnld+"'>   <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQe0yTrscxvCtQSSYecldTwJhb446m5cG2BGRy4gsa2AEKBJWQR' width='30'></img></a>";
+            //console.log(string);
             item.insertAdjacentHTML("afterEnd",string);
+
         }
 
     }
     console.log(count);
-
-
-
 }();
